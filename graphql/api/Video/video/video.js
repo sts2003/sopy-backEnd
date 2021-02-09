8be6d53105d2f4fef54169aa4fce244a7f21d79b
sts2003 <44109148+sts2003@users.noreply.github.com>
2021-02-09 23:37:05 +0900

    sopy

--- graphql/api/Video/video/video.js
+++ graphql/api/Video/video/video.js
@@ -12,5 +12,66 @@ export default {
         return [];
       }
     },
+    getVideoOne: async (_, args) => {
+      const { id } = args;
+      try {
+        const result = await Video.findOne({ _id: id });
+
+        return result;
+      } catch (e) {
+        console.log(e);
+        return {};
+      }
+    },
+  },
+
+  Mutation: {
+    registVideo: async (_, args) => {
+      const { title, description, thumbNailPath } = args;
+      try {
+        const result = await Video.create({
+          title,
+          description,
+          thumbNailPath,
+        });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+
+    updateVideo: async (_, args) => {
+      const { id, title, description, thumbNailPath } = args;
+      try {
+        const result = await Video.updateOne(
+          { _id: id },
+          {
+            $set: {
+              title,
+              description,
+              thumbNailPath,
+            },
+          }
+        );
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+
+    deleteVideo: async (_, args) => {
+      const { id } = args;
+      try {
+        const result = await Video.deleteOne({ _id: id });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
   },
 };
